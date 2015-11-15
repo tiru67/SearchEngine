504c415692de046b9c69c01830b9ea3bc191138c
tiru <tirumalarao.kavala@capitalone.com>
2015-11-15 08:18:10 -0500

    loading files from a directory and reading their content

--- src/SearchEngine.cpp
+++ src/SearchEngine.cpp
@@ -7,9 +7,44 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "dirent.h"
+
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir ("./texts_project")) != NULL) {
+	  /* print all the files and directories within directory */
+	  while ((ent = readdir (dir)) != NULL) {
+	    printf ("%s\n", ent->d_name);
+	    const string dir_name = "./texts_project";
+	    const string file_name = ent->d_name;
+	    const string full_file_name = dir_name + "/" + file_name;
+		 string line;
+		  ifstream myfile (full_file_name.c_str());
+		  if (myfile.is_open())
+		  {
+		    while ( getline (myfile,line) )
+		    {
+		      cout << line << '\n';
+
+		    }
+		    myfile.close();
+		  }
+
+		  else cout << "Unable to open file";
+	  }
+	  closedir (dir);
+	} else {
+	  /* could not open directory */
+	  perror ("");
+	  return EXIT_FAILURE;
+	}
+
+	  return 0;
 }
